a0ec0552360552b8757b029b7de8b3395c2cc493
Adnan <101324354+Adnanoff029@users.noreply.github.com>
2025-01-26 04:51:23 +0530

    Just added the things I have to build for the portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 function App() {
   return (
     <>
+      {/* 
+        Navbar -> Education, Tech Stack, Projects, Experience, Contact (It should be sticky)
+        Header -> Name, Summary
+        Education -> List of institutions and their correspondin website and degree names (for 10th, 12th and Btech.)
+        Tech Stack -> List of technologies, Soft Skills, Languages (Hindi and English)
+        Projects -> List of projects and their corresponding information (Title, Description, Tech Stack, Link (Github and deployed))
+        Experience -> List of employers and their corresponding information (as an expandable list) (Company Name, Position, Duration, Description and my work)
+        Contact -> Email, Linkedin, Github
+        Footer -> Just a made with love by me
+        */
+      }
       <h1 className="text-3xl bg-blue-800">Portfolio</h1>
     </>
   );
